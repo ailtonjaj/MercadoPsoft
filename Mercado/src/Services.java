7da665f8df2e3927770c87dd61a57eed85f7e698
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2023-03-20 13:06:46 -0300

    Update Services.java

--- Mercado/src/Services.java
+++ Mercado/src/Services.java
@@ -3,6 +3,7 @@ public class Services {
 	
 	public void criaProduto(String nome, String fornecedor, double preço) {
 		Produto prod = new Produto(nome, fornecedor, preço);
-		
+		Repositorio repo = new Repositorio();
+		repo.inserir(nome, prod);
 	}
 }
