ab11fe0acaf3a3288888229db31d2ee8401a18ed
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2023-03-20 13:05:30 -0300

    Update Repositorio.java

--- Mercado/src/Repositorio.java
+++ Mercado/src/Repositorio.java
@@ -2,5 +2,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Repositorio {
+	private Map<String, Produto> produtos = new HashMap<String, Produto>();
 	
+	public inserir(String id, Produto prod) {
+		produtos.put(id, prod);
+	}
 }
